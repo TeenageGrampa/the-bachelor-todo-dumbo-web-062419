67e494b960fe577c075bd65170d2ded8ae578f6f
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-20 16:03:27 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,11 +10,19 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  
+  data.each do |season, array|
+    array.each do |contestant|
+      contestant.each do |k,v|
+        if v == occupation
+          return contestant["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  
 end
 
 def get_occupation(data, hometown)
